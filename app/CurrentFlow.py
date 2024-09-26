562382274e5014693ee09d0db904aa7dce3bdf79
daviseemann <daviseemann@gmail.com>
2024-09-26 18:37:48 -0300

    save the legacy bearer token archtecture

--- app/CurrentFlow.py
+++ app/CurrentFlow.py
@@ -1,16 +1,17 @@
 from utils.auth import authenticate
 from utils.http_requests import get_ssl_context
 import json
-import pandas as pd 
+import pandas as pd
 import numpy as np
 
 token = authenticate()
 http = get_ssl_context()
 
-test = http.request("GET",
-        "https://integra.ons.org.br/api/energiaagora/GetBalancoEnergetico/null",
-        headers={"Authorization": f"Bearer {token['access_token']}"},
-    )
+test = http.request(
+    "GET",
+    "https://integra.ons.org.br/api/energiaagora/GetBalancoEnergetico/null",
+    headers={"Authorization": f"Bearer {token['access_token']}"},
+)
 data = test.data.decode("utf-8")
 data_json = json.loads(data)
 
